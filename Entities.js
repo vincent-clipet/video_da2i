98176a8a94468df41ac974f2bdde2487a3fd5f1a
vincent-clipet <vincent.clipet.7@gmail.com>
2014-03-23 00:37:55 +0100

    ADD ::: start of fading-in & fading-out CLEAN ::: beautifying

--- Entities.js
+++ Entities.js
@@ -3,25 +3,28 @@ function Sprite(x, y, imageName, cropCoord, currSprite)
 	this.x = x;
 	this.y = y;
 	this.image = new Image();
-    this.image.src = imageName;
+	this.image.src = imageName;
 	this.cropCoord = cropCoord;
 	this.currSprite = currSprite;
-	
-	this.move = function(dx, dy)
+
+	this.move = function (dx, dy)
 	{
 		this.x += dx;
 		this.y += dy;
 	}
-	
-	this.nextSprite = function()
+
+	this.update = function (frame)
 	{
-		this.currSprite++;
-		
-		if (this.currSprite >= this.cropCoord.length)
-			this.currSprite = 0;
+		if (frame % 4 == 0)
+		{
+			this.currSprite++;
+
+			if (this.currSprite >= this.cropCoord.length)
+				this.currSprite = 0;
+		}
 	}
-	
-	this.draw = function(ctx)
+
+	this.draw = function (ctx)
 	{
 		ctx.drawImage(
 			this.image,
@@ -37,9 +40,9 @@ function Sprite(x, y, imageName, cropCoord, currSprite)
 function Background(imageName)
 {
 	this.image = new Image();
-    this.image.src = imageName;
-	
-	this.draw = function(ctx, offsetX, offsetY)
+	this.image.src = imageName;
+
+	this.draw = function (ctx, offsetX, offsetY)
 	{
 		ctx.drawImage(this.image, offsetX, offsetY);
 	};
@@ -52,40 +55,40 @@ function SchoolTree(x, y, sy, imageName, anims)
 	this.y = y;
 	this.sy = sy;
 	this.image = new Image();
-    this.image.src = imageName;
+	this.image.src = imageName;
 	this.anims = anims;
-	
-	this.scroll = function(dy)
+
+	this.scroll = function (dy)
 	{
 		this.sy -= dy;
 	}
-	
-	this.move = function(dx, dy)
+
+	this.move = function (dx, dy)
 	{
 		this.x += dx;
 		this.y += dy;
 	}
-	
-	this.update = function(frame)
-	{	
+
+	this.update = function (frame)
+	{
 		for (var i = 0; i < anims.length; i++)
 		{
 			var a = this.anims[i];
 			a.update(frame);
-			
+
 			if (a.isInProgress(frame))
 				this.scroll(a.speed);
 		}
 	}
-	
-	this.draw = function(ctx)
+
+	this.draw = function (ctx)
 	{
 		ctx.drawImage(
 			this.image,
-			0, 1760 + this.sy,
-			300, 640,
+			0, 1290 + this.sy,
+			400, 640,
 			this.x, this.y,
-			300, 640
+			400, 640
 		)
 	};
 };
@@ -97,14 +100,49 @@ function Anim(start, end, speed)
 	this.end = end;
 	this.speed = speed;
 	this.val = 0.0;
-	
-	this.update = function(frame)
+
+	this.update = function (frame)
 	{
 		if (this.isInProgress(frame))
 			this.val += this.speed;
 	}
-	
-	this.isInProgress = function(frame)
+
+	this.isInProgress = function (frame)
+	{
+		return (frame >= this.start && frame < this.end);
+	}
+};
+
+
+function Fade(start, end, timeStopped)
+{
+	this.start = start;
+	this.end = end;
+	this.val = 0.0;
+	this.timeStopped = timeStopped;
+	var duration = this.end - this.start - this.timeStopped;
+	this.interval = 1.0 / duration;
+
+	this.update = function (frame)
+	{
+		if (this.isInProgress(frame))
+		{
+			if (frame < this.start + duration) // Fade out
+				this.val += this.interval;
+			else if (frame < this.start + duration + this.timeStopped) // Time stopped
+				this.val = 1.0;
+			else // Fade in
+				this.val -= this.interval;
+		}
+	}
+
+	this.draw = function (ctx, scrollX)
+	{
+		ctx.fillStyle = "rgba(0,0,0," + this.val + ")";
+		ctx.fillRect(-scrollX, 0, 1280, 640);
+	}
+
+	this.isInProgress = function (frame)
 	{
 		return (frame >= this.start && frame < this.end);
 	}
--- Main.js
+++ Main.js
@@ -1,123 +1,122 @@
-        //////////
-        // INIT //
-        //////////
-
-
-        // Canvas
+         //////////
+         // INIT //
+         //////////
+         // Canvas
         var canvas = document.getElementById("da2i");
         canvas.focus();
+        window.onload = function () {
+            pause = false;
+        };
         var ctx = canvas.getContext("2d");
 
-        var pause = false;
+        var pause = true;
         var frame = 0;
-		var scrollX = 0;
-		var scrollY = 0;
-		var speed = 0.5;
+        var scrollX = 0;
+        var scrollY = 0;
+        var speed = 0.5;
+        var frameInterval = 10;
 
-		// Entities
-		//var boy = new Sprite(370, canvas.height - 19 - 44, "images/spritesBoy.png", [ [0, 0, 22, 44], [22, 0, 21, 44], [43, 0, 21, 44], [64, 0, 22, 44] ]);
-		//var girl = new Sprite(400, canvas.height - 19 - 41, "images/spritesGirl.png", [ [0, 0, 20, 41], [20, 0, 20, 41], [40, 0, 19, 41], [59, 0, 19, 41] ]);
-		var boy = new Sprite(210, canvas.height - 19 - 87, "images/spritesBoy.png", [ [0, 0, 43, 88], [43, 0, 43, 87], [86, 0, 43, 88], [129, 0, 43, 87] ], 0);
-		var girl = new Sprite(260, canvas.height - 19 - 81, "images/spritesGirl.png", [ [0, 0, 39, 81], [39, 0, 40, 81], [79, 0, 39, 81], [118, 0, 38, 81] ], 1);
-		var bg = new Background("images/background.png");
-		
-		var anims = new Array();
-		anims[0] = new Anim(0, 145, 3);
-		anims[1] = new Anim(560, 660, 2);
-		anims[2] = new Anim(1150, 1230, 2);
-		anims[3] = new Anim(1650, 1800, 2);
-		var tree = new SchoolTree(980, 0, 0,  "images/tree.png", anims);
-		
+         // Entities
+        var boy = new Sprite(210, canvas.height - 19 - 87, "images/spritesBoy.png", [
+            [0, 0, 43, 88],
+            [43, 0, 43, 87],
+            [86, 0, 43, 88],
+            [129, 0, 43, 87]
+        ], 0);
+        var girl = new Sprite(260, canvas.height - 19 - 81, "images/spritesGirl.png", [
+            [0, 0, 39, 81],
+            [39, 0, 40, 81],
+            [79, 0, 39, 81],
+            [118, 0, 38, 81]
+        ], 1);
+        var bg = new Background("images/background.png");
+
+        var anims = new Array();
+        anims[0] = new Anim(60, 160, 2);
+        anims[1] = new Anim(450, 560, 1.2);
+        anims[2] = new Anim(1150, 1240, 1.2);
+        anims[3] = new Anim(1650, 1800, 1.4);
+        var tree = new SchoolTree(880, 0, 0, "images/tree.png", anims);
+
+        var fades = new Array();
+        fades[0] = new Fade(2200, 2280, 20);
 
 
 
 
-        ////////////
-        // UPDATE //
-        ////////////
+         ////////////
+         // UPDATE //
+         ////////////
 
 
-        
 
-        function update()
-		{
-            if (! pause)
-            {
+
+        function update() {
+            if (!pause) {
                 frame++;
-				
-				if (frame % 20 == 0)
-					console.log("frame = " + frame);
-				
-				if (frame % 4 == 0)
-				{
-					boy.nextSprite();
-					girl.nextSprite();
-				}
-				
-				scrollX -= speed;
+
+                if (frame % 20 == 0) // DEBUG
+                    console.log("frame = " + frame);
+
+                scrollX -= speed;
                 boy.move(speed, 0);
-				girl.move(speed, 0);
-				tree.move(speed, 0);
-				tree.update(frame);
+                boy.update(frame);
+                girl.move(speed, 0);
+                girl.update(frame);
+                tree.move(speed, 0);
+                tree.update(frame);
+
+                for (var key in fades)
+                    fades[key].update(frame);
             }
-			
+
             draw();
         }
 
 
 
 
+         ////////////
+         // RENDER //
+         ////////////
 
 
-
-        ////////////
-        // RENDER //
-        ////////////
-
-
-        function draw()
-		{
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
+        function draw() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.save();
-			ctx.translate(scrollX, scrollY);
-			
-			bg.draw(ctx, scrollX, scrollY);
-			tree.draw(ctx);
-			boy.draw(ctx);
-			girl.draw(ctx);
-			
+            ctx.translate(scrollX, scrollY);
+
+            bg.draw(ctx, scrollX, scrollY);
+            tree.draw(ctx);
+            boy.draw(ctx);
+            girl.draw(ctx);
+
+            for (var key in fades)
+                fades[key].draw(ctx, scrollX);
+
             ctx.restore();
         }
 
 
 
 
-
-
-
-        ///////////////
-        // LISTENERS //
-        ///////////////
+         ///////////////
+         // LISTENERS //
+         ///////////////
 
 
         canvas.addEventListener('keydown', function (e) {
             if (e.keyCode === 32)
-                pause = ! pause;
+                pause = !pause;
             draw();
         }, false);
 
 
 
 
+         /////////
+         // RUN //
+         /////////
 
-
-
-        /////////
-        // RUN //
-        /////////
-
-        var myTimer = setInterval(update, 50);
+        var myTimer = setInterval(update, frameInterval);
         draw();
-        
-        
-        
