2dd3c8988cb959f4eb5393b7ed0b157e11d58702
vincent-clipet <vincent.clipet.7@gmail.com>
2014-03-22 22:07:22 +0100

    ADD ::: 2nd scrolling ADD ::: TODO file

--- Entities.js
+++ Entities.js
@@ -1,25 +1,28 @@
-function Sprite(x, y, imageName, cropCoord)
+function Sprite(x, y, imageName, cropCoord, currSprite)
 {
 	this.x = x;
 	this.y = y;
 	this.image = new Image();
     this.image.src = imageName;
 	this.cropCoord = cropCoord;
-	this.currSprite = 0;
+	this.currSprite = currSprite;
 	
-	this.move = function(dx, dy) {
+	this.move = function(dx, dy)
+	{
 		this.x += dx;
 		this.y += dy;
 	}
 	
-	this.nextSprite = function() {
+	this.nextSprite = function()
+	{
 		this.currSprite++;
 		
 		if (this.currSprite >= this.cropCoord.length)
 			this.currSprite = 0;
 	}
 	
-	this.draw = function(ctx) {
+	this.draw = function(ctx)
+	{
 		ctx.drawImage(
 			this.image,
 			this.cropCoord[this.currSprite][0], this.cropCoord[this.currSprite][1],
@@ -36,7 +39,73 @@ function Background(imageName)
 	this.image = new Image();
     this.image.src = imageName;
 	
-	this.draw = function(ctx, offsetX, offsetY) {
+	this.draw = function(ctx, offsetX, offsetY)
+	{
 		ctx.drawImage(this.image, offsetX, offsetY);
 	};
+};
+
+
+function SchoolTree(x, y, sy, imageName, anims)
+{
+	this.x = x;
+	this.y = y;
+	this.sy = sy;
+	this.image = new Image();
+    this.image.src = imageName;
+	this.anims = anims;
+	
+	this.scroll = function(dy)
+	{
+		this.sy -= dy;
+	}
+	
+	this.move = function(dx, dy)
+	{
+		this.x += dx;
+		this.y += dy;
+	}
+	
+	this.update = function(frame)
+	{	
+		for (var i = 0; i < anims.length; i++)
+		{
+			var a = this.anims[i];
+			a.update(frame);
+			
+			if (a.isInProgress(frame))
+				this.scroll(a.speed);
+		}
+	}
+	
+	this.draw = function(ctx)
+	{
+		ctx.drawImage(
+			this.image,
+			0, 1760 + this.sy,
+			300, 640,
+			this.x, this.y,
+			300, 640
+		)
+	};
+};
+
+
+function Anim(start, end, speed)
+{
+	this.start = start;
+	this.end = end;
+	this.speed = speed;
+	this.val = 0.0;
+	
+	this.update = function(frame)
+	{
+		if (this.isInProgress(frame))
+			this.val += this.speed;
+	}
+	
+	this.isInProgress = function(frame)
+	{
+		return (frame >= this.start && frame < this.end);
+	}
 };
--- Main.js
+++ Main.js
@@ -17,10 +17,17 @@
 		// Entities
 		//var boy = new Sprite(370, canvas.height - 19 - 44, "images/spritesBoy.png", [ [0, 0, 22, 44], [22, 0, 21, 44], [43, 0, 21, 44], [64, 0, 22, 44] ]);
 		//var girl = new Sprite(400, canvas.height - 19 - 41, "images/spritesGirl.png", [ [0, 0, 20, 41], [20, 0, 20, 41], [40, 0, 19, 41], [59, 0, 19, 41] ]);
-		var boy = new Sprite(210, canvas.height - 19 - 87, "images/spritesBoy.png", [ [0, 0, 43, 88], [43, 0, 43, 87], [86, 0, 43, 88], [129, 0, 43, 87] ]);
-		var girl = new Sprite(260, canvas.height - 19 - 81, "images/spritesGirl.png", [ [0, 0, 39, 81], [39, 0, 40, 81], [79, 0, 39, 81], [118, 0, 38, 81] ]);
-		
+		var boy = new Sprite(210, canvas.height - 19 - 87, "images/spritesBoy.png", [ [0, 0, 43, 88], [43, 0, 43, 87], [86, 0, 43, 88], [129, 0, 43, 87] ], 0);
+		var girl = new Sprite(260, canvas.height - 19 - 81, "images/spritesGirl.png", [ [0, 0, 39, 81], [39, 0, 40, 81], [79, 0, 39, 81], [118, 0, 38, 81] ], 1);
 		var bg = new Background("images/background.png");
+		
+		var anims = new Array();
+		anims[0] = new Anim(0, 145, 3);
+		anims[1] = new Anim(560, 660, 2);
+		anims[2] = new Anim(1150, 1230, 2);
+		anims[3] = new Anim(1650, 1800, 2);
+		var tree = new SchoolTree(980, 0, 0,  "images/tree.png", anims);
+		
 
 
 
@@ -38,6 +45,9 @@
             {
                 frame++;
 				
+				if (frame % 20 == 0)
+					console.log("frame = " + frame);
+				
 				if (frame % 4 == 0)
 				{
 					boy.nextSprite();
@@ -45,9 +55,10 @@
 				}
 				
 				scrollX -= speed;
-                //bg.scroll(-speed, 0);
                 boy.move(speed, 0);
 				girl.move(speed, 0);
+				tree.move(speed, 0);
+				tree.update(frame);
             }
 			
             draw();
@@ -71,6 +82,7 @@
 			ctx.translate(scrollX, scrollY);
 			
 			bg.draw(ctx, scrollX, scrollY);
+			tree.draw(ctx);
 			boy.draw(ctx);
 			girl.draw(ctx);
 			
