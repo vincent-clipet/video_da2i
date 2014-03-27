26727696c9c024efd0e8beee9dafcb5705993f7e
Alex-D <demodealex@gmail.com>
2014-03-27 02:59:04 +0100

    ADD :: index page with little design

---
+++ js/video-scripts.prepros.js
@@ -0,0 +1,15 @@
+// @prepros-append Anim.js
+// @prepros-append Background.js
+// @prepros-append FadeIn.js
+// @prepros-append FadeOut.js
+// @prepros-append SchoolTree.js
+// @prepros-append Slide.js
+// @prepros-append Sprite.js
+// @prepros-append TextSign.js
+
+// @prepros-append Part1.js
+// @prepros-append Part2.js
+// @prepros-append Part3.js
+// @prepros-append Part4.js
+
+// @prepros-append Main.js
